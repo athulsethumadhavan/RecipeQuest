539e1ac38e2c740e1af5709f78512c165a4116d9
Athul T S <athulsethumadhavan@gmail.com>
2026-06-24 07:00:28 +0300

    Initial commit

---
+++ ios/Runner/Runner-Bridging-Header.h
@@ -0,0 +1,8 @@
+// Runner-Bridging-Header.h
+// Recipe Quest
+//
+// This header bridges Objective-C Flutter plugin APIs into Swift.
+// Flutter plugins that ship ObjC headers (e.g., google_mobile_ads) are
+// imported here so they're visible to Swift code without explicit imports.
+
+#import "GeneratedPluginRegistrant.h"
